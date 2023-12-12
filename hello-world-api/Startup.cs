c1c6a4ab15097f726eff1ee0acbe48e6f9d7afd1
Paramesh.rajappan <paramesh.rajappan@winwire.com>
2023-12-12 09:12:49 +0530

    updated csprojand startup

--- hello-world-api/Startup.cs
+++ hello-world-api/Startup.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-
+﻿[9:11 AM] Mac
 namespace hello_world_api
 {
     public class Startup
@@ -17,24 +7,42 @@ namespace hello_world_api
         {
             Configuration = configuration;
         }
-
+ 
         public IConfiguration Configuration { get; }
-
+ 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            //services.AddMvc();
+            services.AddControllers();
         }
-
+ 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            /*
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+ 
             app.UseMvc();
+            */
+ 
+             if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+ 
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseCors();
+ 
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
+ 
